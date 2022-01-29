b236bb51b1c96d1692e78b95106c1efcdd931a9f
Brendan Lensink <blensink192@gmail.com>
2022-01-29 09:50:09 -0800

    Update canthresh.js

--- js/canthresh.js
+++ js/canthresh.js
@@ -8,3 +8,15 @@ function tap(img) {
   let audio = new Audio(`canthresh/${img.id}.mp3`);
   audio.play();
 }
+
+function actually(img) {
+  img.src = img.src.replace("up", "down")
+
+  setTimeout(function() {
+      img.src = img.src.replace("down", "up")
+  }, 250);
+  
+  const options = ["jeff_dragons.mp3", "jeff_loch.mp3"]
+  let audio = new Audio(`canthresh/${options[Math.floor(Math.random()*items.length)]}.mp3`);
+  audio.play();
+}
