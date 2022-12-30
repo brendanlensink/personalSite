d7a3cdd9e288abaa38df539870507da203218511
Brendan Lensink <blensink192@gmail.com>
2022-12-30 12:06:05 -0800

    add jekyll, add top 10 2022

---
+++ _site/js/canthresh.js
@@ -0,0 +1,22 @@
+function tap(img) {
+  img.src = img.src.replace("up", "down")
+
+  setTimeout(function() {
+      img.src = img.src.replace("down", "up")
+  }, 250);
+  
+  let audio = new Audio(`canthresh/${img.id}.mp3`);
+  audio.play();
+}
+
+function actually(img) {
+  img.src = img.src.replace("up", "down")
+
+  setTimeout(function() {
+      img.src = img.src.replace("down", "up")
+  }, 250);
+  
+  const options = ["jeff_dragons", "jeff_loch"]
+  let audio = new Audio(`canthresh/${options[Math.floor(Math.random()*options.length)]}.mp3`);
+  audio.play();
+}
